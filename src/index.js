6f71ab4e6e338743e8c2ac485e74b57ab7868f34
devferx <ferkki@hotmail.com>
2021-02-24 16:29:37 -0400

    [ADD] animations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./styles/tailwind.css";
 
 // Your JavaScript Code
+const elementsToAnimate = document.querySelectorAll(".animate");
+
+function animateElement(element) {
+  const observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      const el = entries[0].target;
+      el.classList.add("active");
+      observer.disconnect();
+    }
+  }, {});
+
+  observer.observe(element);
+}
+
+elementsToAnimate.forEach(animateElement);
